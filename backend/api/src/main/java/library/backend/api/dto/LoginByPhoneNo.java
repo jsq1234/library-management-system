553293a4b12e8a7d1e188ae85846883b24ebd31f
jsq1234 <mananprakash3078@gmail.com>
2024-03-01 11:36:25 +0530

    added dto for login by phone_no

---
+++ backend/api/src/main/java/library/backend/api/dto/LoginByPhoneNo.java
@@ -0,0 +1,15 @@
+package library.backend.api.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record LoginByPhoneNo(
+        @NotNull(message = "Expected email field.") 
+        @NotBlank(message = "Email cannot be blank.")
+        @Pattern(regexp = "\\d{10}", message = "Phone number should be 10 digits")
+        String phoneNo,
+        @NotNull(message = "Expected password field.") 
+        @NotBlank(message = "Password cannot be blank.") 
+        String password) {
+}
